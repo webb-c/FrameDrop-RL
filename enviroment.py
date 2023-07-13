2f63ed819cec8ab3a4581f4647a98acccfd36720
vaughn <jwst0208@gmail.com>
2023-07-13 22:30:41 +0900

    add : replay buffer

--- enviroment.py
+++ enviroment.py
@@ -2,3 +2,20 @@
 represents the environment in which the model interacts; 
 it uses replay buffers because we using offline-learning.
 """
+import collections
+import random
+
+random.seed(42)
+class ReplayBuffer():
+    def __init__(self, buffer_size):
+        self.buffer = collections.deque(maxlen=buffer_size)
+        
+    def put(self, transition) :
+        self.buffer.append(transition)
+    
+    def get(self) :
+        transition = random.sample(self.buffer, 1)  # buffer size = 1
+        return transition
+    
+    def size(self) :
+        return len(self.buffer)
