3864ff7b19fb353eae26d5da6b847d88bcca63ec
vaughn <jwst0208@gmail.com>
2023-07-13 23:22:25 +0900

    add : agent and environment skeleton code

--- enviroment.py
+++ enviroment.py
@@ -19,3 +19,20 @@ class ReplayBuffer():
     
     def size(self) :
         return len(self.buffer)
+    
+class FrameEnv():
+    def __init__(self, fps=30, alpha=0.7, beta=10, w=5):
+        self.fps = fps
+        self.alpha = alpha
+        self.alpha = beta
+        self.w = w
+        self.actionSpace = list(range(fps+1)) # 0 to fps
+        
+    def reset(self) : 
+        return
+    
+    def step(self, action) :
+        return
+    
+    def get_reward(self) :
+        return
