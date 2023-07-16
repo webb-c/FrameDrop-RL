cf1dda96c86035e959350d64dcd9fdc230306838
webb-c <jwst0208@gmail.com>
2023-07-16 18:04:43 +0900

    add : state dim change

--- agent.py
+++ agent.py
@@ -8,25 +8,26 @@ from enviroment import FrameEnv
 
 random.seed(42)
 
+
 class Agent():
     def __init__(self, eps=0.9, fps=30):
         self.eps = eps
-        self.Qtable = np.zeros((10, 3, 30))  # s1(img), s2(net), a 
-        self.actionSpace = list(range(fps+1)) # 0 to fps
-        
+        self.Qtable = np.zeros((30, 30))  # s, a
+        self.actionSpace = list(range(fps+1))  # 0 to fps
+
     def get_action(self, s):
         s1, s2 = s
         p = random.random()
-        if p < self.eps : # exploration
+        if p < self.eps:  # exploration
             action = random.sample(self.actionSpace, 1)
-        else : # exploitation
+        else:  # exploitation
             Qvalue = self.Qtable[s1, s2, :]
             action = np.argmax(Qvalue)
         return action
-    
+
     def Q_update(self):
         return
-    
+
     def decrese_eps(self):
         self.eps -= 0.01
         self.eps = max(self.eps, 0.1)
