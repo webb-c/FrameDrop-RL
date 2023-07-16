a08fc04136b0e7b7aee124e575370a0f2293d501
vaughn <jwst0208@gmail.com>
2023-07-16 16:36:26 +0900

    add : clustering

--- utils/get_state.py
+++ utils/get_state.py
@@ -1,3 +1,16 @@
 """
 clustering for 3-dimension continuous state to (10,3) discrete state 
 """
+import joblib
+from sklearn import KMeans
+
+def cluster_train(data, k=30) :
+    model = KMeans(n_cluster=k)  # else : MiniBatchKMeans
+    model.fit(data)
+    joblib.dump(model, '../models/cluster.pt')
+    
+def get_state(sMSE, sFFT, sNet):
+    env = [sMSE, sFFT, sNet]
+    model = joblib.load("../models/cluster.pt")
+    s = model.predict(env)
+    return s
