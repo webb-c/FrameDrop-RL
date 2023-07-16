e05045ddf3c55b9c2b64b7c0e434794912f50187
vaughn <jwst0208@gmail.com>
2023-07-16 18:00:12 +0900

    add : cal MSE, blurring

--- utils/cal_quality.py
+++ utils/cal_quality.py
@@ -3,3 +3,28 @@ The quality of the current frame is evaluated in the following two aspects.
     1. Redundancy
     2. Blurring
 """
+import numpy as np
+import cv2
+
+# redundancy
+def get_MSE(prev_frame, frame) :
+    diff = cv2.absdiff(prev_frame, frame)
+    MSE = np.mean(np.square(diff))
+    return MSE
+    
+# blurring
+def get_FFT(frame, radius=60) :
+    H, W = frame.shape
+    centerX, centerY = map(int, [W/2.0, H/2.0])
+    # fft
+    fft = np.fft.fft2(frame)
+    fftShift = np.fft.fftshift(fft)
+    # remove Blur
+    fftShift[centerY - radius : centerY + radius, centerX - radius, centerX + radius] = 0
+    # ifft
+    fftShift = np.fft.ifftshift(fftShift)
+    reFrame = np.fft.ifft2(fftShift)
+    # magnitude
+    magnitude = 20*np.log(np.abs(reFrame))
+    magMean = np.mean(magnitude)
+    return magMean
