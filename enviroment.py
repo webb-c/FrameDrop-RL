5123e99a4583501fb2051acbdf93501e4ab3e865
webb-c <jwst0208@gmail.com>
2023-07-18 04:08:55 +0900

    add : step develop

--- enviroment.py
+++ enviroment.py
@@ -16,19 +16,21 @@ class ReplayBuffer():
     def __init__(self, buffer_size):
         self.buffer = collections.deque(maxlen=buffer_size)
 
-    def put(self, transition):
-        self.buffer.append(transition)
+    def put(self, transList):
+        for trans in transList:
+            self.buffer.append(trans)
 
     def get(self):
-        transition = random.sample(self.buffer, 1)  # buffer size = 1
-        return transition
+        trans = random.sample(self.buffer, 1)  # batch size = 1
+        return trans
 
     def size(self):
         return len(self.buffer)
 
 
 class FrameEnv():
-    def __init__(self, videoPath, fps=30, alpha=0.7, beta=10, w=5):
+    def __init__(self, videoPath, buffer_size=10000, fps=30, alpha=0.7, beta=10, w=5):
+        self.buffer = ReplayBuffer(buffer_size)
         self.videoPath = videoPath
         self.fps = fps
         # hyper-parameter
@@ -42,25 +44,69 @@ class FrameEnv():
             get_MSE(self.prev_frame, self.frame), get_FFT(self.frame), self.net, self.model)
 
     def reset(self):
+        self.transList = []
         self.cap = cv2.VideoCapture(self.videoPath)
+        self.prevA = self.fps
         self.targetA = self.fps
         self.net = 0
-        _, self.prev_frame = self.cap.read()
-        _, self.frame = self.cap.read()
+        _, f1 = self.cap.read()
+        _, f2 = self.cap.read()
+        self.frameList = [f1, f2]
+        self.processList = [f1, f2]
+        self.prev_frame = self.frameList[-2]
+        self.frame = self.frameList[-1]
 
-    def step(self, action, isGuided, targetA):
-        for _ in range(action-1):
-            self.cap.read()
-        _, self.prev_frame = self.cap.read()
-        ret, self.frame = self.cap.read()
-        if isGuided:
-            self.targetA = targetA
-            self.net = max(self.fps - self.targetA, 0)
-        else:
+    def step(self, action, newA):
+        # skipping
+        guided = False
+        duplicated = False
+        for a in range(action+1):
+            _, temp = self.cap.read()
+            if len(self.frameList) >= self.fps:
+                guided = True
+                duplicated = self._triggered_by_guide(newA, temp, action, a)
+            else:
+                self.frameList.append(temp)
+
+        self.prev_frame = self.frameList[-2]
+        self.frame = self.frameList[-1]
+
+        if not guided:
             self.net = max(self.net - action, 0)
-        self.state = cluster_pred(
-            get_MSE(self.prev_frame, self.frame), get_FFT(self.frame), self.net, self.model)
+            self.transList.append((self.state, action))
+
+        if not duplicated:
+            self.processList.append(self.frame)
+            self.state = cluster_pred(
+                get_MSE(self.prev_frame, self.frame), get_FFT(self.frame), self.net, self.model)
+
         return self.state
 
-    def get_reward(self):
+    def _triggered_by_guide(self, newA, temp, action, a):
+        # subTask 1
+        self.transList.append((self.state, a))
+        self._get_reward()
+        self.buffer.put(self.transList)
+        # init
+        self.transList = []
+        self.prevA = self.targetA
+        self.targetA = newA   # OMNeT ++
+        self.net = max(self.fps - self.targetA, 0)
+        self.frameList = [self.frameList[-1], temp]
+        self.processList = [temp]
+        # subTask 2
+        na = action-a-1
+        self.state = cluster_pred(
+            get_MSE(self.frameList[-2], self.frameList[-1]), get_FFT(self.frameList[-1]), self.net, self.model)
+        if na >= 0:
+            self.transList = [(self.state, na)]
+            self.net = max(self.fps - self.targetA - na, 0)
+            return False
+        return True
+
+    def _get_reward(self):
+        length = len(self.transList)
+        for i in range(length):
+            s, a = self.transList[i]
+            # request addition (YOLO -> self.frameList detect)
         return
